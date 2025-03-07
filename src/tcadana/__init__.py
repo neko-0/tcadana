72f4c707a929c7b021565ff14de64382cc781876
neko-0 <yuzhan.physics@gmail.com>
2025-03-06 19:13:40 -0500

    Included serialization base for importing from module

--- src/tcadana/__init__.py
+++ src/tcadana/__init__.py
@@ -5,9 +5,11 @@ __getattr__, __dir__, __all__ = lazy.attach(
     submodules=[
         'parser',
         'triangle_tools',
+        'json_tools',
     ],
     submod_attrs={
         'version': ['__version__'],
         'parser': ['open_tdr'],
+        'serialization.base': ['SerilizationBase'],
     },
 )
